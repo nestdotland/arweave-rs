3ffb68b80687191030f55b6f7e339ae769513470
littledivy <dj.srivastava23@gmail.com>
2021-01-22 19:42:00 +0530

    feat(tx): implement get_price to transactions

--- src/tx.rs
+++ src/tx.rs
@@ -1,13 +1,15 @@
 use serde::Deserialize;
 
-#[derive(Deserialize, Debug)]
+use crate::api::{Api, Error};
+
 pub struct Tag {
     pub name: String,
     pub value: String,
 }
 
-#[derive(Deserialize, Debug)]
 pub struct Transaction<'a> {
+    api: Api<'a>,
+
     pub format: usize,
     pub id: &'a str,
     pub last_tx: &'a str,
@@ -22,8 +24,27 @@ pub struct Transaction<'a> {
     pub data_root: &'a str,
 }
 
+/// A new Transaction instance.
+///
+/// Has various methods for interacting Arweave transactions.
 impl<'a> Transaction<'a> {
     pub fn add_tag(&mut self, name: String, value: String) {
         self.tags.push(Tag { name, value })
+    }
+
+    pub async fn get_price(
+        &self,
+        byte_size: &'a str,
+        target_address: Option<&'a str>,
+    ) -> Result<String, Error> {
+        let endpoint = match target_address {
+            Some(addr) => format!("price/{}/{}", byte_size, addr),
+            None => format!("price/{}", byte_size),
+        };
+
+        // XXX: Investigate whether it will convert Winston to Integer.
+        //      It really shouldn't because serde doesn't type cast like JSON.parse
+        //      But just in case, yk.
+        self.api.get::<String>(&endpoint).await
     }
 }
