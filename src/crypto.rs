9c00080f2a905236605bda50ec4e20bb08a40a15
littledivy <dj.srivastava23@gmail.com>
2021-01-25 13:43:47 +0530

    fix: crypto

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,5 +1,5 @@
 use aes::Aes256;
-use block_modes::{BlockMode, Cbc, block_padding::{Pkcs7}};
+use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
 use sha2::{Digest, Sha256, Sha384};
 
 type Aes256Cbc = Cbc<Aes256, Pkcs7>;
@@ -7,10 +7,8 @@ type Aes256Cbc = Cbc<Aes256, Pkcs7>;
 const RSA_KEY_LENGTH: u32 = 4096;
 const PUBLIC_EXPONENT: u32 = 0x10001;
 
-fn hash_SHA256(data: &[u8]) -> &[u8] {
+fn hash_SHA256(data: Vec<u8>) -> Vec<u8> {
     let mut hasher = Sha256::new();
-
     hasher.update(data);
-
-    hasher.finalize()[..]
+    hasher.finalize().as_slice().to_vec()
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 pub mod api;
+pub mod crypto;
 pub mod tx;
 pub mod wallet;
 pub mod winston;
-pub mod crypto;
 
 mod b64;
 mod macros;
