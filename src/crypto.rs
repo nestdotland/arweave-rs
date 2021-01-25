92e5676d0f93ee6d24e28498cafa63911a6eea8d
littledivy <dj.srivastava23@gmail.com>
2021-01-25 20:36:30 +0530

    chores(crypto): create Crypto struct

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,3 +1,4 @@
+use crate::jwk;
 use aes::Aes256;
 use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
 use hmac::Hmac;
@@ -5,88 +6,95 @@ use rand::RngCore;
 use rsa::{PaddingScheme, PublicKey};
 use sha2::{Digest, Sha256};
 
-pub fn hash<H: Digest>(data: &[u8]) -> Vec<u8> {
-    let mut hasher = H::new();
-    hasher.update(data);
-    hasher.finalize().as_slice().to_vec()
-}
+pub struct Crypto;
 
-pub fn sign(
-    key: &str,
-    data: &[u8],
-    salt_len: usize,
-) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let private_key = crate::jwk::read_private_jwk(key)?;
+impl Crypto {
+    pub fn hash<H: Digest>(data: &[u8]) -> Vec<u8> {
+        let mut hasher = H::new();
+        hasher.update(data);
+        hasher.finalize().as_slice().to_vec()
+    }
 
-    let pad = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::ThreadRng>(
-        rand::thread_rng(),
-        salt_len,
-    );
+    pub fn sign(
+        key: &str,
+        data: &[u8],
+        salt_len: usize,
+    ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let private_key = jwk::read_private_jwk(key)?;
 
-    let signature = private_key.sign(pad, &data)?;
+        let pad = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::ThreadRng>(
+            rand::thread_rng(),
+            salt_len,
+        );
 
-    Ok(signature.as_slice().to_vec())
-}
+        let signature = private_key.sign(pad, &data)?;
 
-pub fn verify(
-    key: &str,
-    data: Vec<u8>,
-    signature: Vec<u8>,
-) -> Result<(), Box<dyn std::error::Error>> {
-    let public_key = crate::jwk::read_public_jwk(key)?;
+        Ok(signature.as_slice().to_vec())
+    }
 
-    let pad = PaddingScheme::new_pss::<Sha256, rand::rngs::ThreadRng>(rand::thread_rng());
+    pub fn verify(
+        key: &str,
+        data: Vec<u8>,
+        signature: Vec<u8>,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let public_key = crate::jwk::read_public_jwk(key)?;
 
-    public_key.verify(pad, &hash::<Sha256>(&data), &signature)?;
+        let pad = PaddingScheme::new_pss::<Sha256, rand::rngs::ThreadRng>(rand::thread_rng());
 
-    Ok(())
-}
+        public_key.verify(pad, &Crypto::hash::<Sha256>(&data), &signature)?;
 
-pub fn derive_password(password: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let mut out = [0u8; 32];
+        Ok(())
+    }
 
-    pbkdf2::pbkdf2::<Hmac<Sha256>>(password.as_bytes(), b"salt", 100000, &mut out);
+    pub fn derive_password(password: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let mut out = [0u8; 32];
 
-    Ok(Vec::from(out))
-}
+        pbkdf2::pbkdf2::<Hmac<Sha256>>(password.as_bytes(), b"salt", 100000, &mut out);
 
-pub fn encrypt(password: &str, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let key = derive_password(password)?;
-    let mut iv = [0u8; 32];
+        Ok(Vec::from(out))
+    }
 
-    rand::thread_rng().fill_bytes(&mut iv);
+    pub fn encrypt(password: &str, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let key = Crypto::derive_password(password)?;
+        let mut iv = [0u8; 32];
 
-    let cipher = Cbc::<Aes256, Pkcs7>::new_var(&key, &iv)?;
+        rand::thread_rng().fill_bytes(&mut iv);
 
-    let output_data = cipher.encrypt_vec(data);
+        let cipher = Cbc::<Aes256, Pkcs7>::new_var(&key, &iv)?;
 
-    let mut output_vecs = Vec::<&[u8]>::new();
+        let output_data = cipher.encrypt_vec(data);
 
-    output_vecs.push(&iv);
-    output_vecs.push(&output_data);
+        let mut output_vecs = Vec::<&[u8]>::new();
 
-    Ok(output_vecs.concat())
-}
+        output_vecs.push(&iv);
+        output_vecs.push(&output_data);
 
-pub fn decrypt(password: &str, ciphertext: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
-    let key = derive_password(password)?;
-    let iv = &ciphertext[0..16];
+        Ok(output_vecs.concat())
+    }
 
-    let decipher = Cbc::<Aes256, Pkcs7>::new_var(&key, &iv)?;
+    pub fn decrypt(
+        password: &str,
+        ciphertext: &[u8],
+    ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let key = Crypto::derive_password(password)?;
+        let iv = &ciphertext[0..16];
 
-    let output_data = decipher.decrypt_vec(&ciphertext[16..])?;
+        let decipher = Cbc::<Aes256, Pkcs7>::new_var(&key, &iv)?;
 
-    Ok(output_data)
+        let output_data = decipher.decrypt_vec(&ciphertext[16..])?;
+
+        Ok(output_data)
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::crypto::*;
+    use crate::crypto::{Crypto, Sha256};
 
     #[test]
     fn test_hash() {
         assert_eq!(
-            hash::<Sha256>(b"hello"),
+            Crypto::hash::<Sha256>(b"hello"),
             &[
                 44, 242, 77, 186, 95, 176, 163, 14, 38, 232, 59, 42, 197, 185, 226, 158, 27, 22,
                 30, 92, 31, 167, 66, 94, 115, 4, 51, 98, 147, 139, 152, 36,
@@ -97,6 +105,6 @@ mod tests {
     #[test]
     #[should_panic]
     fn test_encrypt_fail_ivlen() {
-        encrypt("test_password", &[1, 5, 6, 23, 7, 10, 34]).unwrap();
+        Crypto::encrypt("test_password", &[1, 5, 6, 23, 7, 10, 34]).unwrap();
     }
 }
