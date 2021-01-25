9106378b39bb89bba81baa886fee076f75bcfdf2
littledivy <dj.srivastava23@gmail.com>
2021-01-25 20:30:31 +0530

    feat(crypto): add unit tests

--- src/crypto.rs
+++ src/crypto.rs
@@ -1,9 +1,9 @@
 use aes::Aes256;
 use block_modes::{block_padding::Pkcs7, BlockMode, Cbc};
+use hmac::Hmac;
+use rand::RngCore;
+use rsa::{PaddingScheme, PublicKey};
 use sha2::{Digest, Sha256};
-use hmac::{Hmac};
-use rsa::{PublicKey, PaddingScheme};
-use rand::{RngCore};
 
 pub fn hash<H: Digest>(data: &[u8]) -> Vec<u8> {
     let mut hasher = H::new();
@@ -11,17 +11,28 @@ pub fn hash<H: Digest>(data: &[u8]) -> Vec<u8> {
     hasher.finalize().as_slice().to_vec()
 }
 
-pub fn sign(key: &str, data: &[u8], salt_len: usize) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+pub fn sign(
+    key: &str,
+    data: &[u8],
+    salt_len: usize,
+) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
     let private_key = crate::jwk::read_private_jwk(key)?;
 
-    let pad = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::ThreadRng>(rand::thread_rng(), salt_len);
+    let pad = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::ThreadRng>(
+        rand::thread_rng(),
+        salt_len,
+    );
 
     let signature = private_key.sign(pad, &data)?;
 
     Ok(signature.as_slice().to_vec())
 }
 
-pub fn verify(key: &str, data: Vec<u8>, signature: Vec<u8>) -> Result<(), Box<dyn std::error::Error>> {
+pub fn verify(
+    key: &str,
+    data: Vec<u8>,
+    signature: Vec<u8>,
+) -> Result<(), Box<dyn std::error::Error>> {
     let public_key = crate::jwk::read_public_jwk(key)?;
 
     let pad = PaddingScheme::new_pss::<Sha256, rand::rngs::ThreadRng>(rand::thread_rng());
@@ -66,4 +77,26 @@ pub fn decrypt(password: &str, ciphertext: &[u8]) -> Result<Vec<u8>, Box<dyn std
     let output_data = decipher.decrypt_vec(&ciphertext[16..])?;
 
     Ok(output_data)
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::crypto::*;
+
+    #[test]
+    fn test_hash() {
+        assert_eq!(
+            hash::<Sha256>(b"hello"),
+            &[
+                44, 242, 77, 186, 95, 176, 163, 14, 38, 232, 59, 42, 197, 185, 226, 158, 27, 22,
+                30, 92, 31, 167, 66, 94, 115, 4, 51, 98, 147, 139, 152, 36,
+            ]
+        );
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_encrypt_fail_ivlen() {
+        encrypt("test_password", &[1, 5, 6, 23, 7, 10, 34]).unwrap();
+    }
 }
--- src/jwk.rs
+++ src/jwk.rs
@@ -1,69 +1,69 @@
-use serde::{Serialize, Deserialize};
-use serde_json::{from_str};
 use rsa::{BigUint, RSAPrivateKey, RSAPublicKey};
+use serde::{Deserialize, Serialize};
+use serde_json::from_str;
 
 #[derive(Serialize, Deserialize)]
 struct JwkPublic {
-  kty: String,
-  n: Vec<u8>,
-  e: Vec<u8>,
+    kty: String,
+    n: Vec<u8>,
+    e: Vec<u8>,
 }
 
 #[derive(Serialize, Deserialize)]
 struct JwkPrivate {
-  kty: String,
-  n: Vec<u8>,
-  e: Vec<u8>,
-  d: Vec<u8>,
-  p: Vec<u8>,
-  q: Vec<u8>,
-  dp: Vec<u8>,
-  dq: Vec<u8>,
-  qi: Vec<u8>,
+    kty: String,
+    n: Vec<u8>,
+    e: Vec<u8>,
+    d: Vec<u8>,
+    p: Vec<u8>,
+    q: Vec<u8>,
+    dp: Vec<u8>,
+    dq: Vec<u8>,
+    qi: Vec<u8>,
 }
 
-pub fn read_public_jwk (data: &str) -> Result<RSAPublicKey, Box<dyn std::error::Error>> {
-  let components: JwkPublic = from_str(&data)?;
+pub fn read_public_jwk(data: &str) -> Result<RSAPublicKey, Box<dyn std::error::Error>> {
+    let components: JwkPublic = from_str(&data)?;
 
-  if components.kty != "RSA" {
-    // TODO: return error
-    panic!("invalid key type")
-  }
+    if components.kty != "RSA" {
+        // TODO: return error
+        panic!("invalid key type")
+    }
 
-  let e = BigUint::from_bytes_be(&components.e);
-  let n = BigUint::from_bytes_be(&components.n);
+    let e = BigUint::from_bytes_be(&components.e);
+    let n = BigUint::from_bytes_be(&components.n);
 
-  let public_key = RSAPublicKey::new(e, n)?;
+    let public_key = RSAPublicKey::new(e, n)?;
 
-  Ok(public_key)
+    Ok(public_key)
 }
 
-pub fn read_private_jwk (data: &str) -> Result<RSAPrivateKey, Box<dyn std::error::Error>> {
-  let components: JwkPrivate = from_str(&data)?;
+pub fn read_private_jwk(data: &str) -> Result<RSAPrivateKey, Box<dyn std::error::Error>> {
+    let components: JwkPrivate = from_str(&data)?;
 
-  if components.kty != "RSA" {
-    // TODO: return error
-    panic!("invalid key type")
-  }
+    if components.kty != "RSA" {
+        // TODO: return error
+        panic!("invalid key type")
+    }
 
-  let n = BigUint::from_bytes_be(&components.n);
-  let e = BigUint::from_bytes_be(&components.e);
-  let d = BigUint::from_bytes_be(&components.d);
-  let p = BigUint::from_bytes_be(&components.p);
-  let q = BigUint::from_bytes_be(&components.q);
-  let dp = BigUint::from_bytes_be(&components.dp);
-  let dq = BigUint::from_bytes_be(&components.dq);
-  let qi = BigUint::from_bytes_be(&components.qi);
+    let n = BigUint::from_bytes_be(&components.n);
+    let e = BigUint::from_bytes_be(&components.e);
+    let d = BigUint::from_bytes_be(&components.d);
+    let p = BigUint::from_bytes_be(&components.p);
+    let q = BigUint::from_bytes_be(&components.q);
+    let dp = BigUint::from_bytes_be(&components.dp);
+    let dq = BigUint::from_bytes_be(&components.dq);
+    let qi = BigUint::from_bytes_be(&components.qi);
 
-  let mut primes = Vec::<BigUint>::new();
+    let mut primes = Vec::<BigUint>::new();
 
-  primes.push(p);
-  primes.push(q);
-  primes.push(dp);
-  primes.push(dq);
-  primes.push(qi);
+    primes.push(p);
+    primes.push(q);
+    primes.push(dp);
+    primes.push(dq);
+    primes.push(qi);
 
-  let private_key = RSAPrivateKey::from_components(n, e, d, primes);
+    let private_key = RSAPrivateKey::from_components(n, e, d, primes);
 
-  Ok(private_key)
+    Ok(private_key)
 }
