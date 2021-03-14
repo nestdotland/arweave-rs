2bcb189ccefd9b9241cd2e8fcacfde06e323e940
littledivy <dj.srivastava23@gmail.com>
2021-03-14 21:32:56 +0530

    fix(crypto): signing issues with private key

--- src/crypto.rs
+++ src/crypto.rs
@@ -10,8 +10,9 @@ use sha2::{Digest, Sha256};
 pub type CryptoResult = Result<Vec<u8>, Box<dyn std::error::Error>>;
 
 pub trait CryptoInterface {
+    // TODO: async fn generateKey()
     fn hash<H: Digest>(data: &[u8]) -> Vec<u8>;
-    fn sign(key: JwkPrivate, data: &[u8], salt_len: usize) -> CryptoResult;
+    fn sign(key: JwkPrivate, data: &[u8], salt_len: Option<usize>) -> CryptoResult;
     fn verify(
         key: JwkPublic,
         data: Vec<u8>,
@@ -28,24 +29,23 @@ impl CryptoInterface for Crypto {
     fn hash<H: Digest>(data: &[u8]) -> Vec<u8> {
         let mut hasher = H::new();
         hasher.update(data);
-        hasher.finalize().as_slice().to_vec()
+        hasher.finalize().to_vec()
     }
 
     fn sign(
         key: JwkPrivate,
         data: &[u8],
-        salt_len: usize,
+        salt_len: Option<usize>,
     ) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+        let salt_len = if let Some(salt) = salt_len { salt } else { 32 };
+
         let private_key = jwk::read_private_jwk(key)?;
+        let rng = rand::thread_rng();
+        let pad = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::ThreadRng>(rng, salt_len);
 
-        let pad = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::ThreadRng>(
-            rand::thread_rng(),
-            salt_len,
-        );
+        let signature = private_key.sign_blinded(&mut rng.clone(), pad, &data)?;
 
-        let signature = private_key.sign(pad, &data)?;
-
-        Ok(signature.as_slice().to_vec())
+        Ok(signature.to_vec())
     }
 
     fn verify(
@@ -64,27 +64,19 @@ impl CryptoInterface for Crypto {
 
     fn derive_password(password: &str) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         let mut out = [0u8; 32];
-
         pbkdf2::pbkdf2::<Hmac<Sha256>>(password.as_bytes(), b"salt", 100000, &mut out);
-
         Ok(Vec::from(out))
     }
 
     fn encrypt(password: &str, data: &[u8]) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
         let key = Crypto::derive_password(password)?;
         let mut iv = [0u8; 32];
-
         rand::thread_rng().fill_bytes(&mut iv);
-
         let cipher = Cbc::<Aes256, Pkcs7>::new_var(&key, &iv)?;
-
         let output_data = cipher.encrypt_vec(data);
-
         let mut output_vecs = Vec::<&[u8]>::new();
-
         output_vecs.push(&iv);
         output_vecs.push(&output_data);
-
         Ok(output_vecs.concat())
     }
 
@@ -125,16 +117,16 @@ mod tests {
     fn test_sign_verify() {
         // https://github.com/ArweaveTeam/arweave-js/blob/master/test/fixtures/arweave-keyfile-fOVzBRTBnyt4VrUUYadBH8yras_-jhgpmNgg-5b3vEw.json
         let wallet: JwkPrivate = JwkPrivate {
-            kty: "RSA".to_string(),
+            d: b"F2UEjuzxUuqCaE0UEurZTxdyRI65Hgmz0maPAU9b1hgGlxKXZ4JW8q4PJHKWXqffdNptrlY9Y85t0yUOuPkmH7ChMbxpPKxffjq9qnMvbUn8Ye46hnQsqS093lWY-qdhy37TmXEC9eGxfpRpPKvbw-F7L2mVoV3gyzS99yJNtNuXhxgBMsN8ApSdpmB0QzUonix4jFiLZ6rYQzYh4tqnxkZnIg2HgrEo2kJ-l7TkRZFvbH2X2aPwmplJ7aKa0aBfad_MizxLsc6yU7z16lcr6qzUZhiW5SZoVegDan5gRIgyICVyklwtl9N8pT41ESk_iMweWofBiuCIIB7Gu_9wN4Ee_oe15ZmwACKqdsMUH6Xs2FPNRnqEs6Wc-5H1-6yuHxOE3X6XznsqCOvzkJH-DGFB8si1JGaHbxluY8ExJf_IcuOhGEzRsCOEP8jQJpNByUufbSMYIKau3hlRVdA3duKeGzCT-LEKang7DbIBnoenMt32xtHp1t6dNWPlRbynS8l8irpoEi3b-Xd7ERVuQMckArIhmefK5T99xJJGpwIX-lOcuCtnYuSCk-Qtc2R8-Jcg5fM_tdkgNA9724MBa5WYxXqnj8sFlwrCoCXu7ofag3SsLWiuoGCAgYtGA-sSAzwXo_Lu47VMiZqdjAgjNhhEshb8-Ss1BXk8OwPbMfk".to_vec(),
+            dp: b"GqbwEzo9_1MFEQkw9pm3yW5LqJCP6bERxxzad_M1i9oyDtvOUZn-lBYSW4qOioXjsAYorpqQPgSj8t1eEyU1JY8O49ED092D8JCniFqzvDB7PRpID5TxRhPvoFD6Pom3PgVvJwUgL5VF3W56bV4WEmdhSWxPQsBbpz5lix6LteFJOxqAcry_6Fw0nQ2GFeBag58L1VqZwIwcyq9n0ftT50UwxbhFedrf-cfFTuyMONTPFCqQZTdRDcTrZU51sEjLj8k4-RZQSmeFq5bbIr-TEhGka6PwfyWRbnCmPqagOj2FAeUxRhHKTjbkBDVhfm5iT7TyBisEcfbC4CfOuvECGQ".to_vec(),
+            dq: b"WoqQuHnl2y3UxGBPyvydCZcAyyzTdC7_ThZyXzouBYuEFSZ5pICVZDLG14Jm8uJ1A1vL52iFeiv_greYGwsp5keijQdPrJjHGW2mC6pMGoIBI_jhoUIzgUwkYdEp1lIH-TW8YcZ5GMFH7ZlVbCMtk8l_fDuoz2KpCMZChBR2vOP9ZKL7DqFJ9IUEi6raGOSIeny-VRlQOPmnEgRoZB9GJBfmliGbNocvqEWYkoM6bq27pi9QIiILWsIcLa3NSzCRm2h-f3_SDr7cN7AUZOwvcD2NCuc2NCJM4a3FstU9eUbaweQjNBvWuUG2saDgeHPvdOKuMSqkCnnc7sVeZnUriQ".to_vec(),
             e: b"AQAB".to_vec(),
-            n: b"kmM4O08BJB85RbxfQ2nkka9VNO6Czm2Tc_IGQNYCTSXRzOc6W9bHRrlZ_eDhWO0OdfaRalgLeuYCXx9DV-n1djeerKHdFo2ZAjRv5WjL_b4IxbQnPFnHOSNHVg49yp7CUWUgDQOKtylt3x0YENIW37RQPZJ-Fvyk7Z0jvibj2iZ0K3K8yNenJ4mWswyQdyPaJcbP6AMWvUWT62giWHa3lDgBZNhXqakkYdoaM157kRUfrZDRSWXbilr-4f40PQF1DV5YSj81Fl72N7j30r0vL1yoj0bZn74WRquQ5j3QsiAA-SzhAxpecWniljj1wvZlyIgJpCYCvCrKZCcCq_JW1nYP6to5YM3fAqcYRadbTNdQ3oH0Sjy8vyvLYNe48Ur_TFTTAwZxJV70BgZfkJ00BxiNTb8EhSchejabeExUkCNlOrQsCHDxOig-WXOrjX5fb4NeR3jedeYWbhN922ORLuEwVLeyjc7hBfQXU2-mYraFAVTc0QST201P7rRu-UGtZ4gRavFuOvAyYrMimFVW9dTwTrcYXFK2zKCEv2aRRQAHZanKjBv0Xq9m3BqvxKy-_3Cj1O6ft7FT21drPoDRDzfnkyOeUjlXzRJzn-iQ0nqgHAQr9WBWPzLEcaTFpw3KmwDYHW_6JOkUWDyMW9anuS8cyqt_2O29SK_rHHuucD8".to_vec(),
-            d: b"Bq6C13vknF6Ln1MrKI3Ilq-83IuSvQpe7NRAuT69u7i8sv4XwsHOJAV7qpGvp37NXT5R1G3ehEZ6qoSxJbcN4IVrQMKq5mMiCY6DBv5C6fHZGoNZE2gxXV7uydf8I1Vnnw4xYIj5oyC_5nSJlFAc3U-MAcbkfJuvrhGxLVGsrqHmjoqQPGG_hTxCjuAOlOBs-9cmWTujbm1-OyjaAQwfTbXYbUy7hC1TCE05SxLPmTUwaJxY8AXJigpbYqjpWsc15HjRlv38A44tEnIwHjHda_3JpmbSsffSslRej2vPCCgSPHHyLeO437Nc7DraogKStugisRfhoe89yY4QSBVXbtvWJeF1LxPtg8uPtfoKt3wdnGWKaLDqYNDeA3AckbKrPp50kHEMR7hnNHq3lAoMAXTz8BbI_Czo5n9-f9DQpvJC8kpM7gCGG8DptA2nTPuQG02MOx7AsEE99EN8ltD_dA0l0MgG7CDsaQC5IPMHcRs1wyvZMBGA8fvZdURiVv9YSnCddndXjBJuetf5KdES-1EmrSLzo5hobQbkc7dkHMS5dmLm5YtK-aLYXZi31nRIGkA1UfZhf2TtfRxP6uKlRT106EtDX1rT3RgsLqg06y_xoS4SFQ6u-8wHqgbIHBmKdsWVtBkC4SGUlYDPgrJe2V9CaPFAcoSDFK1D_IPvU2U".to_vec(),
-            p: b"zhyauK0ISMg9Wk7iZK2ifW2cj5KSr-_k_Em0nfUrtsaKp0iXOsCKxOH__zcAVj7oLxaEP2l8i2Pdi7CzhVRiqrjgVwA1JuLPgxtryuVqwRCYbO_Y_2Xutk404iKmDX6_LQ7BeIzUI8GD6rQCeLq1HBd3Yvok9bPvbbMZjFtUmBf_Kfb0cYP8tewMmV_USGpqwJXdB_4aHF6qBrZBtd1KLoO1E7MNkAPk7pbiA1-KO2Xa6oY6fy2pztNe1MO7tz_QywqlDdymfhnpk41arY3A6US-ZFXOinqXKdh9uEfxiyZwzaLMNWVKEaWxRxbqSUOLV3uZS05N6B2ZqvOp2h9Csw".to_vec(),
-            q: b"tdHmYcbJpZ0U27d_j0YvUeb6sdcuFLg2vmDgKwUamC5_vvV41LSm8LIkLuY2DAN5MKg6-HTWetKWQhgbCIbubLtX5164MFrES1YVZI-aggrYohhH8MRn_hwMZZQndv9H07WUVgQ1GZ2ZDvhO7XxPDIXyBNQ46x6V1AikHtyTmqARjgrkgs-1XN55S9rhcffixOlJ-egIDPVei_Z6YNdSpLlhtiqHOp_lX37mrPSYGxjgIZVxevpPgBhVFlnAMqC2iRd87XupmWgiluSos8I7i1VESBzwlFZGk5hRb8och4zwmDBDwx65XWngg6LneSXTWcKjKKGM2NnX7wHrZBuyRQ".to_vec(),
-            dp: b"Gfo49fW5CZNTSEKQ_id0R2K9TMsoecw-jB2uCgqQi-TSLOtVRC5oTxA896my_SvIj8bCvEtLSzY3AhgvSCqulN3gSJbaHCCSDvAx0czAe7zfuTsxml76izeoKqg7TZAgAEnP0KXPRwJo4ff2J8lAcl3yyiLE7cLT9nuQSMRqERFVM7DQdk4wV618mQge9VGUStmYlh1MpS65N0dZWNafNuWauPTkTLZw8DFMIyizf3EC-nQYg1b6A_tYBHD3A82jPzQEQY8B3PrfGZ3DRASNv9jONk8qTQHOc5O5pLRMmUErDn_qRQCTKU483bzhooJE2a3WUEt6Pjsc1xMG4Vr3SQ".to_vec(),
-            dq: b"cCVai36Yi-06m1cwd8fbkhH9GUpXIvKI2Z5ZRk-smqc7piY0dEZFHftS9BaMyZYu3wM09GDklfdkNLo3mmfXkftv-cbjpvelUa50HYWx0HouKrT9UpVia0sTnmfme7BztjKunuuTcQxTBvfDfxoIi_nmUHIx9Vv1IEaALITzChGnIky3q7O_8ttKR65nFevG1JvsRBeJN6z0tzG9RBQr5mxtx3Wt2Uwcp21XjOCFHVmXjT9nMmpINQNNIC8VrGSSkjaJmNWIw5WGmDnLkKzCG2vpZO1suqIIgCsYN_Ka7ETTdZt3gFdoECUpFSiay4-4MAospvgWLv8XAFXXwfSPXQ".to_vec(),
-            qi: b"n-R81MpbwfWfqRSVgD8nDk7D8zlJ-tpMaojfTwNNqDt34Cr-BpMjxaQyEfMnzOd2dY4OV0rKhd29DIuwFEb2UERHdVWF3gM8f2byYGj4357CRkiwq6I050bUxd1ODgAXjVGNpOK_fmaNHDWfe5v3wVIcCmwH0mJxEu9kuz7fr9TJNxGJBGUphpGS6NQZDCbDXg9-FPafMeNV-Jdo0NQaKMwm8uZyW7YGSNpUXYnksrWt4Fa-B9H2KoC4PPSWESPxNooXdxK7Y0J1KbzNyrUmOl4dT6p_oFKcU-1unuDCZ11e6EmMKyUGjpDzTIAZ2XxmyWUJ06yzEw7oLo8noiCE_Q".to_vec(),
+            kty: "RSA".to_string(),
+            n: b"lTBMnymUu-q1w3S2cNDAWn3t0NhcL5k3xqqrIN2HOQP6TE3H57s1wZnD1IAbiBbew2QgZu0WJWkJR-Mmotu7xHvf4A45EiZQ0rHOD2pjNBJ5m8cEbRLI9SlmywDOD81LX-TgGNNYrUpKfNi2XjykTMD6t2gTNFLayVxe2qqnhhKxLUy0hFbBgKA6EKR4wVZCSzSaXNeD2pdmtI62N10N8qcU3GuM691QXr3JGndq__fBLNaiC5pS0HGzzGZXcJkSqeqWe2zOG5wqAlLkTr4ele1LYDO84tAilTg0eRtx4t3qIzZL-H7wrjKUO45eSyLqjbqt405xgXWGgVrq-GghD2KbaTgQfHLEtUICATBEARkvYGvpe0jZxqQW25qF55mcGimht_V2mD6BahJ4u4No4uZ5c9qWo2yhLvcupT1e2sQNpls8HQXVHySGlUUhJKj2pdeefdMiwh6zES0wePqkGjWqs20b5db5U_9jI7q5PxoACpLPkXIzpq0iKptK9qs4LsA-3t1d76RdL3ZNjVcIf_pxivQnqz4mWJOt6MzN0WXM7oe2HQLq1QVVCxbXko-hIvWVBDFWWj3tBNuImaYKiVex85OisfqqsQI5MKPvsG8mmytN3-JM6G2PmKaf49nQyc2xMwfcf1oKBXFluL3crmkiMYoHXkZXoSkGou1bM9M".to_vec(),
+            p: b"xJFHGzSGpHkgNeGFCR-WEn9757D1-022XlcMwcMynqyuRPc334MWeT-ftTy7hb7W25SeeTVDmoBQ4edAQnipeo-ZSn-Ovze2MVa4EDKXgVw7KZY-26vmvBo1Pzmih0mhYenogTBvOXzmyIXm37DQ0wqmio-4hpYbwsCzH2efU_qsaA09SM6U6zPxSS4DNZkarT8xKQl7OM7jyjCWT0JCGxzAwsgHb7uaCKKwZBRfV1JzJnqg-vqXdAsYqtXaDPpoE39quAN_L2oxhskNeKyQGBrsCxK644nIdK4kvZc3gwSHu5zBsWGPLsNkScrPMQevR15BqOkV3aLpjhuCeGpk5Q".to_vec(),
+            q: b"wkvN_8DzM11ydKjBw0fr7yKFqwP6gnOSqx4DVV8Z2WL1x2xTHJ6cn83ulWtQ4njXLQmMwF0vzXMOCch_kcgL8t6CvP1S0V0kqqOwJJ4OufC9sdNRdjmCj5JWjoxRvccNVqfDTpWRJN6Ca-jSnOyq7R1u3TBb1IHl4CQJ4JuI6dwTQFjIyjiaa3zoTNhh8DrBdW580DeR6EbQuGBALjOFTyW-VQh-iuaJIeAnXWmPSe9BLmWDndfPbAuXp0v4S-S_OGGWcrmRI3LdSNRHIAabv92kiNZU7Oz_Qx2I1xWyQ_e36szGkMYM_Db9CzqYGjCvO80x0vFkIRwgZSUpLFKiVw".to_vec(),
+            qi: b"G-mOGewvS8rYyBoHUFPKwbAxXRL3PlnjIFhwvv15PnMaH5xqHJc0m_lO8nTJoPH1qznQHF0JOfZL0k5CMxoVVX0mtM4JjYz-7jUmlRHwSQ9ved41_rrew9yeLtnLRusfeGQWAVQxWAXxWqeSK9tPnbHCMGqsLnNXSZjVXHV99epKaAyKMI3V3qOtITjDVSM2_ajN30SKdPEEzHtEdNV3sSH4IDZEPXt8QQ8HU00W9fWAK7qjq4bs03jsfhe9h7SHifDBgYngfXxcecWqTvTbUbmUgzAjzORCGgAPg5CFdGD0urN8qd3ro3cZfjoAtF5NuRjL3W_sMjY26BqRhmHLeA".to_vec(),
         };
-        Crypto::sign(wallet, &Crypto::hash::<Sha256>(b"hello"), 640).unwrap();
+        Crypto::sign(wallet, &Crypto::hash::<Sha256>(b"hello"), None).unwrap();
     }
 }
