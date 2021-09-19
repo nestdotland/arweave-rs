91fc53415b5b64905ac8b0bfd4e0a878e7d4b31f
Divy Srivastava <dj.srivastava23@gmail.com>
2021-09-19 07:29:28 +0000

    p_key

--- crypto/src/lib.rs
+++ crypto/src/lib.rs
@@ -3,23 +3,56 @@ use rsa::padding::PaddingScheme;
 use rsa::BigUint;
 use rsa::PublicKey;
 use rsa::RsaPrivateKey;
+use sha2::Digest;
 use sha2::Sha256;
 
 pub struct Driver;
 
 /// Wrapper type for private key.
-pub struct Key(RsaPrivateKey);
+pub struct PrivateKey(RsaPrivateKey);
 
-impl Key {
+impl PrivateKey {
     pub fn to_jwk() {
         // Perform Private key (PKCS#1) to JWK conversion.
         unimplemented!()
+    }
+
+
+    pub fn sign(&self, data: &[u8]) -> Vec<u8> {
+        // Perform RSA-PSS signing operation
+        // > saltLength = 32
+        let rng = OsRng;
+        let padding = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::OsRng>(rng, 32);
+        let digest = {
+            let mut hasher = Sha256::new();
+            hasher.update(data);
+            hasher.finalize()
+        };
+        // TODO(@littledivy): Handle operation failures
+        let signature = self.0.sign(padding, &digest).expect("Signing failed");
+        signature
+    }
+
+    pub fn verify(&self, data: &[u8], signature: &[u8]) -> bool {
+        // Perform RSA-PSS verification operation
+        // > saltLength = 32
+        let rng = OsRng;
+        let padding = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::OsRng>(rng, 32);
+        let hashed = {
+            let mut hasher = Sha256::new();
+            hasher.update(data);
+            hasher.finalize()
+        };
+        self.0
+            .to_public_key()
+            .verify(padding, &hashed, &signature)
+            .is_ok()
     }
 }
 
 impl Driver {
     /// Generate a new JWK key.
-    pub fn generate_key() -> Key {
+    pub fn generate_key() -> PrivateKey {
         // Generate an RSA private key
         // > modulusLength = 4096
         // > publicExponent = 65537
@@ -30,29 +63,9 @@ impl Driver {
         let mut rng = OsRng;
         let p_key =
             RsaPrivateKey::new_with_exp(&mut rng, 4096, &exponent).expect("Key generation failed");
-        Key(p_key)
+        PrivateKey(p_key)
     }
 
-    pub fn sign(key: &Key, data: &[u8]) -> Vec<u8> {
-        // Perform RSA-PSS signing operation
-        // > saltLength = 32
-        let rng = OsRng;
-        let padding = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::OsRng>(rng, 32);
-        // TODO(@littledivy): Handle operation failures
-        let signature = key.0.sign(padding, &data).expect("Signing failed");
-        signature
-    }
-
-    pub fn verify(key: &Key, data: &[u8], signature: &[u8]) -> bool {
-        // Perform RSA-PSS verification operation
-        // > saltLength = 32
-        let rng = OsRng;
-        let padding = PaddingScheme::new_pss_with_salt::<Sha256, rand::rngs::OsRng>(rng, 32);
-        key.0
-            .to_public_key()
-            .verify(padding, &data, &signature)
-            .is_ok()
-    }
 }
 
 #[cfg(test)]
@@ -69,10 +82,10 @@ mod tests {
     fn test_sign_verify() {
         let key = Driver::generate_key();
 
-        let signature = Driver::sign(&key, &[0; 16]);
+        let signature = key.sign(&[0; 16]);
         assert!(signature.len() > 0);
 
-        let verified = Driver::verify(&key, &[0; 16], &signature);
+        let verified = key.verify(&[0; 16], &signature);
         assert!(verified);
     }
 }
